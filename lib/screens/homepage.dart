25c930787ca386112a2363bdf73c87f59fb5ad17
Prasad Bhale <prasadbhale000@gmail.com>
2022-06-15 17:04:43 +0530

    Add files via upload

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,88 @@
+import 'package:cryptobase/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:cryptobase/screens/coinCard.dart';
+import 'dart:convert';
+import 'dart:async';
+
+import 'coinModel.dart';
+
+
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<List<Coin>> fetchCoin() async {
+    coinList = [];
+    final response = await http.get(Uri.parse(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> values = [];
+      values = json.decode(response.body);
+      if (values.length > 0) {
+        for (int i = 0; i < values.length; i++) {
+          if (values[i] != null) {
+            Map<String, dynamic> map = values[i];
+            coinList.add(Coin.fromJson(map));
+          }
+        }
+        setState(() {
+          coinList;
+        });
+      }
+      return coinList;
+    } else {
+      throw Exception('Failed to load coins');
+    }
+  }
+
+  @override
+  void initState() {
+    fetchCoin();
+    Timer.periodic(Duration(seconds: 10), (timer) => fetchCoin());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.grey[300],
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.red),
+            onPressed: () {
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+            },
+          ),
+          title: Text(
+            'CRYPTOBASE',
+            style: TextStyle(
+              color: Colors.grey[900],
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: coinList.length,
+          itemBuilder: (context, index) {
+            return CoinCard(
+              name: coinList[index].name,
+              symbol: coinList[index].symbol,
+              imageUrl: coinList[index].imageUrl,
+              price: coinList[index].price.toDouble(),
+              change: coinList[index].change.toDouble(),
+              changePercentage: coinList[index].changePercentage.toDouble(),
+            );
+          },
+        ));
+  }
+}
